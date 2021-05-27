73736d126cf53f534ac822bdef841b3bba44008f
Harsh <hagarwal9200@gmail.com>
2021-05-28 03:25:20 +0530

    Update for tf version 0.12

--- script.tf
+++ script.tf
@@ -6,14 +6,14 @@ terraform {
     }
 }
 
-terraform {
-    required_providers {
-        okta = {
-            source = "okta/okta"
-            version = "~> 3.10"
-        }
-    }
-}
+# terraform {
+#     required_providers {
+#         okta = {
+#             source = "okta/okta"
+#             version = "~> 3.10"
+#         }
+#     }
+# }
 
 provider "okta" {
     org_name  = var.org_name
